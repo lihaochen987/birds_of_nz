d88904961a4c2516b7075048837d16215ef6c0b9
lihaochen987 <69620841+lihaochen987@users.noreply.github.com>
2020-12-28 18:32:54 +1300

    Revert "Adding authentication"
    This reverts commit 239098e3b4d34d876b9f56af20cd287ca79ab449.

--- middleware.js
+++
@@ -1,9 +0,0 @@
-// Not 100% sure what this does
-module.exports.isLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
-    req.flash("error", "You must be signed in first!");
-    return res.redirect("/login");
-  }
-  next();
-};
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,6 @@
 const express = require("express");
 // Preserve req.params from the parent router
 const router = express.Router({ mergeParams: true });
-const reviews = require("../controllers/reviews");
 
 const Bird = require("../models/bird");
 const Review = require("../models/review");
@@ -11,9 +10,9 @@ const { reviewSchema } = require("../schemas.js");
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
-// ONLY USE WHEN WE'RE CREATING SOMETHING (HENCE CHECKING OUR MODEL)
+// Not 100% sure what this does
 const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body); //Check for an error as part of the object we get back
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
@@ -22,8 +21,29 @@ const validateReview = (req, res, next) => {
   }
 };
 
-router.post("/", validateReview, catchAsync(reviews.createReview));
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const bird = await Bird.findById(req.params.id);
+    const review = new Review(req.body.review);
+    bird.reviews.push(review);
+    await review.save();
+    await bird.save();
+    req.flash('success', 'Created new review!');
+    res.redirect(`/birds/${bird._id}`);
+  })
+);
 
-router.delete("/:reviewId", catchAsync(reviews.deleteReview));
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Bird.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted a review!');
+    res.redirect(`/birds/${id}`);
+  })
+);
 
 module.exports = router;
