93a0cdba366a72abeb39c9ce7b08838e984288cc
lihaochen987 <69620841+lihaochen987@users.noreply.github.com>
2020-12-28 18:19:50 +1300

    Revert back

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const flash = require("connect-flash");
 const express = require("express");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user");
 const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const { birdSchema, reviewSchema } = require("./schemas.js");
+const Bird = require("./models/bird");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Review = require("./models/review");
 const session = require("express-session");
-const morgan = require('morgan');
+const flash = require("connect-flash");
 
-const userRoutes = require("./routes/users");
-const birdRoutes = require("./routes/birds");
-const reviewRoutes = require("./routes/reviews");
+const birds = require("./routes/birds");
+const reviews = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/birds-of-nz", {
   useNewUrlParser: true,
@@ -34,7 +33,6 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
@@ -53,23 +51,14 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/", userRoutes);
-app.use("/birds", birdRoutes);
-app.use("/birds/:id/reviews", reviewRoutes);
+app.use("/birds", birds);
+app.use("/birds/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
   res.render("home");
--- routes/birds.js
+++ routes/birds.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const { birdSchema } = require("../schemas.js");
-const { isLoggedIn } = require("../middleware");
-const birds = require("../controllers/birds");
 
 const ExpressError = require("../utils/ExpressError");
 const Bird = require("../models/bird");
@@ -19,17 +17,73 @@ const validatePost = (req, res, next) => {
   }
 };
 
-router.route("/")
-.get(catchAsync(birds.index))
-.post(isLoggedIn, validatePost, catchAsync(birds.createPost));
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const birds = await Bird.find({});
+    res.render("birds/index", { birds });
+  })
+);
 
-router.route("/:id")
-.get(isLoggedIn, catchAsync(birds.showPost))
-.put(isLoggedIn, validatePost, catchAsync(birds.updatePost))
-.delete(isLoggedIn, catchAsync(birds.deletePost));
+router.get("/new", (req, res) => {
+  res.render("birds/new");
+});
 
-router.get("/new", isLoggedIn, birds.renderNewForm);
+router.post(
+  "/",
+  validatePost,
+  catchAsync(async (req, res) => {
+    if (!req.body.bird) throw new ExpressError("Invalid Post Data", 400);
+    const bird = new Bird(req.body.bird);
+    await bird.save();
+    req.flash("success", "Successfully made a new post!");
+    res.redirect(`birds/${bird._id}`);
+  })
+);
 
-router.get("/:id/edit", isLoggedIn, catchAsync(birds.renderEditForm));
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const bird = await Bird.findById(req.params.id).populate("reviews");
+    if (!bird) {
+      req.flash("error", "Cannot find that post");
+      return res.redirect("/birds");
+    }
+    res.render("birds/show", { bird });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const bird = await Bird.findById(req.params.id);
+    if (!bird) {
+      req.flash("error", "Cannot find that post!");
+      return res.redirect("/birdss");
+    }
+    res.render("birds/edit", { bird });
+  })
+);
+
+router.put(
+  "/:id",
+  validatePost,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const bird = await Bird.findByIdAndUpdate(id, { ...req.body.bird });
+    req.flash('success', 'Successfully updated a post');
+    res.redirect(`${bird._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Bird.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted a post');
+    res.redirect("/birds");
+  })
+);
 
 module.exports = router;
