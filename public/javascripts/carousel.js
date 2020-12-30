ab455f40a999b98fffe82513a61216f27241ae46
HaoChen Li <lihaochen987@gmail.com>
2020-12-30 19:09:09 +1300

    Fixed flickity errors
    Removed duplicate javascript code from show.ejs. Changed all the const to var in carousel.js.

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,19 +1,19 @@
-const carousel = document.querySelector('.main-carousel');
-const flkty = new Flickity( carousel, {
+var carousel = document.querySelector('.main-carousel');
+var flkty = new Flickity( carousel, {
   imagesLoaded: true,
   percentPosition: false,
 });
 
-const imgs = carousel.querySelectorAll('.carousel-cell img');
+var imgs = carousel.querySelectorAll('.carousel-cell img');
 // get transform property
-const docStyle = document.documentElement.style;
-const transformProp = typeof docStyle.transform == 'string' ?
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
   'transform' : 'WebkitTransform';
 
 flkty.on( 'scroll', function() {
   flkty.slides.forEach( function( slide, i ) {
-    const img = imgs[i];
-    const x = ( slide.target + flkty.x ) * -1/3;
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
     img.style[ transformProp ] = 'translateX(' + x  + 'px)';
   });
 });
