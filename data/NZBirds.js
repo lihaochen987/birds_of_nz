6fc7270cd4aa3335a69aeb74bdd99af170977353
HaoChen Li <lihaochen987@gmail.com>
2020-12-31 11:53:12 +1300

    Added NZBirds data
    Data taken from native NZ bird wikipedia page

---
+++ data/NZBirds.js
@@ -0,0 +1,258 @@
+module.exports = [
+  {
+    name: "Bellibird",
+    maori: "Korimako",
+    category: "Common Birds",
+  },
+  {
+    name: "Brown creeper",
+    maori: "Pipipi",
+    category: "Common Birds",
+  },
+
+  {
+    name: "Buff-banded rail",
+    maori: "Moho-pereru",
+    category: "Common Birds",
+  },
+  {
+    name: "Dabchick",
+    maori: "Weweia",
+    category: "Common Birds",
+  },
+  {
+    name: "New Zealand falcon",
+    maori: "Kārearea",
+    category: "Common Birds",
+  },
+  {
+    name: "Fantail",
+    maori: "Pīwakawaka",
+    category: "Common Birds",
+  },
+  {
+    name: "Grey warbler",
+    maori: "Riroriro",
+    category: "Common Birds",
+  },
+  {
+    name: "Sacred kingfisher",
+    maori: "Kōtare",
+    category: "Common Birds",
+  },
+  {
+    name: "Long-tailed cuckoo",
+    maori: "Koekoeā",
+    category: "Common Birds",
+  },
+  {
+    name: "New Zealand pigeon",
+    maori: "kererū",
+    category: "Common Birds",
+  },
+  {
+    name: "Pipit",
+    maori: "Pīhoihoi",
+    category: "Common Birds",
+  },
+  {
+    name: "New Zealand dotterel",
+    maori: "Tūturiwhatu",
+    category: "Common Birds",
+  },
+  {
+    name: "Northern New Zealand dotterel",
+    maori: "Tūturiwhatu",
+    category: "Common Birds",
+  },
+  {
+    name: "Southern New Zealand dotterel",
+    maori: "Tūturiwhatu",
+    category: "Common Birds",
+  },
+  {
+    name: "South Island robin",
+    maori: "Kakaruwai",
+    category: "Common Birds",
+  },
+  {
+    name: "North Island robin",
+    maori: "Tarapō",
+    category: "Common Birds",
+  },
+  {
+    name: "New Zealand scaup",
+    maori: "Pāpango",
+    category: "Common Birds",
+  },
+  {
+    name: "Paradise shelduck",
+    maori: "pūtangitangi",
+    category: "Common Birds",
+  },
+  {
+    name: "Pukeko",
+    maori: "Pūkeko",
+    category: "Common Birds",
+  },
+  {
+    name: "Rifleman",
+    maori: "tītipounamu",
+    category: "Common Birds",
+  },
+  {
+    name: "New Zealand rock wren",
+    maori: "Pīwauwau",
+    category: "Common Birds",
+  },
+  {
+    name: "Shining cuckoo",
+    maori: "Pipiwharauroa",
+    category: "Common Birds",
+  },
+  {
+    name: "Silvereye",
+    maori: "Tauhou",
+    category: "Common Birds",
+  },
+  {
+    name: "Spotted shag",
+    maori: "Parekareka",
+    category: "Common Birds",
+  },
+  {
+    name: "Tomtit",
+    maori: "miromiro",
+    category: "Common Birds",
+  },
+  {
+    name: "Tui",
+    maori: "Tūī",
+    category: "Common Birds",
+  },
+  {
+    name: "Whitehead",
+    maori: "Pōpokotea",
+    category: "Common Birds",
+  },
+  {
+    name: "Wrybill",
+    maori: "Ngutuparore",
+    category: "Common Birds",
+  },
+  {
+    name: "Southern brown kiwi",
+    maori: "Tokoeka",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Great spotted kiwi",
+    maori: "roroa",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Little spotted kiwi",
+    maori: "Kiwi pukupuku",
+    category: "Flightless Birds",
+  },
+  {
+    name: "North island brown kiwi",
+    maori: "Roroa",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Okarito kiwi",
+    maori: "Rowi",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Southern brown kiwi",
+    maori: "Tokoeka",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Yellow-eyed penguin",
+    maori: "Hoiho",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Kakapo",
+    maori: "Kākāpō",
+    category: "Flightless Birds",
+  },
+  {
+    name: "South Island Takahē",
+    maori: "Takahē",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Fiordland penguin",
+    maori: "Tawaki",
+    category: "Flightless Birds",
+  },
+  {
+    name: "WekaBlue penguin",
+    maori: "Kororā",
+    category: "Flightless Birds",
+  },
+  {
+    name: "Saddleback",
+    maori: "Tieke",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Fernbird",
+    maori: "Mātātā",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Kaka",
+    maori: "Kākā",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "New Zealand storm petrel",
+    maori: "Takahikare",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "North Island kōkako",
+    maori: "Kōkako",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "South Island kōkako",
+    maori: "Kōkako",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "New Zealand fairy tern",
+    maori: "Tara-iti",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Orange-fronted parakeet",
+    maori: "Kākāriki",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Black stilt",
+    maori: "Kakī",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Blue duck",
+    maori: "Whio",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "Yellowhead",
+    maori: "Mōhua",
+    category: "Endangered endemic birds (flying)",
+  },
+  {
+    name: "KeaStitchbird",
+    maori: "Hihi",
+    category: "Endangered endemic birds (flying)",
+  },
+];
