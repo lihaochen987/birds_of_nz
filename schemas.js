6934719a4f997d218e000391460b243c777d47a7
HaoChen Li <lihaochen987@gmail.com>
2021-02-03 14:26:38 +1300

    Finishing up basic security with sanitize-html

--- schemas.js
+++ schemas.js
@@ -1,18 +1,39 @@
-const Joi = require("joi");
-const { number } = require("joi");
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.birdSchema = Joi.object({
   bird: Joi.object({
-    species: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    species: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array(),
 });
 
 module.exports.commentSchema = Joi.object({
   comment: Joi.object({
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
   }).required(),
 });
