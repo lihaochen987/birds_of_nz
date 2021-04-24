44074c3f75864b00d5a414bab3b278ac49195a1d
HaoChen Li <lihaochen987@gmail.com>
2021-04-24 21:44:20 +1200

    Trying to deply onto Heroku.

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ const commentRoutes = require("./routes/comments");
 const upvotePostRoutes = require("./routes/upvotePost");
 const upvoteCommentRoutes = require("./routes/upvoteComment");
 
-mongoose.connect("mongodb://localhost:27017/birds-of-nz", {
+const MongoDBStore = require("connect-mongo");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/birds-of-nz";
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -57,9 +60,22 @@ app.use(
   })
 );
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
+const store = new MongoDBStore({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -114,23 +130,23 @@ const manifestUrls = [
 
 app.use(
   helmet.contentSecurityPolicy({
-      directives: {
-          defaultSrc: [...manifestUrls],
-          connectSrc: ["'self'", ...connectSrcUrls],
-          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-          workerSrc: ["'self'", "blob:"],
-          objectSrc: [],
-          imgSrc: [
-              "'self'",
-              "blob:",
-              "data:",
-              "https://res.cloudinary.com/dabgwgfzh/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-              "https://images.unsplash.com/",
-              "https://source.unsplash.com/collection/36390090/1600x900"
-          ],
-          fontSrc: ["'self'", ...fontSrcUrls],
-      },
+    directives: {
+      defaultSrc: [...manifestUrls],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dabgwgfzh/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://images.unsplash.com/",
+        "https://source.unsplash.com/collection/36390090/1600x900",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
   })
 );
 
